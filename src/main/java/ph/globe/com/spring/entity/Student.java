ccf0fa3e4f8a46125082d371457ef35fb167e8c0
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-08-17 15:16:16 +0800

    Change the package name from model to entity

---
+++ src/main/java/ph/globe/com/spring/entity/Student.java
@@ -0,0 +1,30 @@
+package ph.globe.com.spring.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Table
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studNo;
+
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate = new Date();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate = new Date();
+
+    private String name;
+    private String username;
+    private String password;
+    private String program;
+
+
+
+}
