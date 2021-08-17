1f28dcd50ef79d648acb2a066295c4b12fb6b2de
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-08-17 15:17:12 +0800

    Minor Changes

--- src/main/java/ph/globe/com/spring/controller/UserController.java
+++ src/main/java/ph/globe/com/spring/controller/UserController.java
@@ -2,7 +2,9 @@ package ph.globe.com.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ph.globe.com.spring.model.Student;
+import ph.globe.com.spring.converter.StudentConverter;
+import ph.globe.com.spring.dto.StudentDto;
+import ph.globe.com.spring.entity.Student;
 import ph.globe.com.spring.repository.StudentJpaRepository;
 
 import java.util.List;
@@ -12,25 +14,27 @@ import java.util.List;
 public class UserController {
     @Autowired
     private StudentJpaRepository studentJpaRepository;
+    @Autowired
+    private StudentConverter studentConverter;
 
     @GetMapping(value = "/all")
-    public List<Student> findAll(){
-        return studentJpaRepository.findAll();
+    public List<StudentDto> findAll(){
+        List<Student> findAll = studentJpaRepository.findAll();
+        return studentConverter.entityToDto(findAll);
     }
 
-    @GetMapping(value = "/{name}")
-    public Student findByName(@PathVariable final String name){
-        return studentJpaRepository.findByName(name);
+    @GetMapping(value = "/{studNo}")
+    public StudentDto findById(@PathVariable(value = "studNo") Long studNo){
+        Student orElse = studentJpaRepository.findById(studNo).orElse(null);
+        return studentConverter.entityToDto(orElse);
     }
 
-    @PostMapping(value = "/load")
-    public Student load(@RequestBody  final Student student){
-
+    @PostMapping(value = "/save")
+    public StudentDto save(@RequestBody StudentDto dto){
+        Student student = studentConverter.dtoToEntity(dto);
         studentJpaRepository.save(student);
-        return studentJpaRepository.findByName(student.getName());
+        return studentConverter.entityToDto(student);
     }
 
-    public Student createStudent(@PathVariable final String name){
-        return studentJpaRepository.findByName(name);
-    }
+
 }
--- src/main/java/ph/globe/com/spring/repository/StudentJpaRepository.java
+++ src/main/java/ph/globe/com/spring/repository/StudentJpaRepository.java
@@ -2,10 +2,9 @@ package ph.globe.com.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-import ph.globe.com.spring.model.Student;
+import ph.globe.com.spring.entity.Student;
 
 @Component
 public interface StudentJpaRepository extends JpaRepository<Student, Long> {
-    Student findByName(String name);
 
 }
